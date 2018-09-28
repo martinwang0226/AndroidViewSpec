0aa2582df0c32813c259e06dfea4c3e595579511
marting.wang <mc3895q@dingtalk.com>
2018-09-28 14:45:24 +0800

    自定义View初始化

---
+++ AndroidViewSpec/app/src/main/java/e/martinwang/androidviewspec/View/CircleView.java
@@ -0,0 +1,46 @@
+package e.martinwang.androidviewspec.View;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by martinwang on 2018/9/28.
+ */
+
+public class CircleView extends View {
+    private int mColor = Color.RED;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public CircleView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CircleView(Context context, AttributeSet attrs) {
+        super(context);
+        init();
+    }
+
+    public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        mPaint.setColor(mColor);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int width = getWidth();
+        int height = getHeight();
+        int radius = Math.min(width, height) / 2;
+        canvas.drawCircle(width / 2, height / 2, radius, mPaint);
+    }
+}
